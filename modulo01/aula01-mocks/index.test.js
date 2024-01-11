23320ef48bc1578e8efb4d444dd0d9e7f983b3a6
Guilherme Alacoc <guilhermealacoc@gmail.com>
2024-01-11 00:05:44 -0300

    adicionando validacao para caso de sucesso

--- modulo01/aula01-mocks/index.test.js
+++ modulo01/aula01-mocks/index.test.js
@@ -19,5 +19,36 @@ const assert = require('assert')
     await assert.rejects(result, expect)
   }
 
+  {
+    const filePath = './mocks/fiveItems-invalid.csv'
+    const expect = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+    const result = File.csvToJson(filePath)
+    await assert.rejects(result, expect)
+  }
 
+  {
+    const filePath = './mocks/threeItems-valid.csv'
+    const expect = [
+      {
+        id: 1,
+        name: "guilherme",
+        profession: "developer",
+        age: 23
+      },
+      {
+        id: 2,
+        name: "jose",
+        profession: "driver",
+        age: 40
+      },
+      {
+        id: 3,
+        name: "maria",
+        profession: "nurse",
+        age: 29
+      }
+    ]
+    const result = await File.csvToJson(filePath)
+    assert.deepEqual(result, expect)
+  }
 })()
