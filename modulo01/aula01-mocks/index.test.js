86e567212a860ec1826fa2fe7616129f48b359fd
Guilherme Alacoc <guilhermealacoc@gmail.com>
2024-06-06 23:16:34 -0300

    feat: correcao de sintaxe errada e finalizacao de estudo de mocks

--- modulo01/aula01-mocks/index.test.js
+++ modulo01/aula01-mocks/index.test.js
@@ -1,13 +1,52 @@
 const { error } = require("./src/constantes");
 const File = require("./src/file");
-const assert = require('assert')
-;(async () => {
-
+const assert = require("assert");
+(async () => {
   //variaveis criadas neste bloco so sao validas durante sua execucao
+  // {
+  //   const filePath = "./mocks/emptyFile-invalid.csv";
+  //   const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+  //   const result = File.csvToJson(filePath);
+  //   await assert.rejects(result, expected);
+  // }
+
+  // {
+  //   const filePath = "./mocks/invalid-header.csv";
+  //   const expected = new Error(error.FILE_FIELDS_ERROR_MESSAGE);
+  //   const result = File.csvToJson(filePath);
+  //   await assert.rejects(result, expected);
+  // }
+
+  // {
+  //   const filePath = "./mocks/fiveItems-invalid.csv";
+  //   const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+  //   const result = File.csvToJson(filePath);
+  //   await assert.rejects(result, expected);
+  // }
+
   {
-    const filePath = './mocks/emptyFile-invalid.csv'
-    const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
-    const result = File.csvToJson(filePath)
-    await assert.rejects(result, expected)
+    const filePath = "./mocks/threeItems-valid.csv";
+    const expected = [
+      {
+        id: 1,
+        name: "jose",
+        profession: "manager",
+        age: 30,
+      },
+      {
+        id: 2,
+        name: "silva",
+        profession: "developer",
+        age: 22,
+      },
+      {
+        id: 3,
+        name: "mario",
+        profession: "sales",
+        age: 41,
+      },
+    ];
+    const result = await File.csvToJson(filePath);
+    assert.deepEqual(result, expected);
   }
-})()
+})();
--- modulo01/aula01-mocks/src/file.js
+++ modulo01/aula01-mocks/src/file.js
@@ -1,28 +1,58 @@
-const {readFile} = require('fs/promises')
-const { error } = require('./constantes')
+const { readFile } = require("fs/promises");
+const { error } = require("./constantes");
 const DEFAULT_OPTION = {
   maxLines: 3,
-  fields: ["id","name","profession","age"]
-}
+  fields: ["id", "name", "profession", "age"],
+};
 class File {
-  static async csvToJson(filePath){
-    const content = await readFile(filePath, "utf8")
-    const validation = this.isvalid(content)
-    if(!validation.valid) throw new Error(validation.error)
+  static async csvToJson(filePath) {
+    const content = await readFile(filePath, "utf8");
+    const validation = this.isvalid(content);
+    if (!validation.valid) throw new Error(validation.error);
+
+    const result = this.parseCsvToJson(content);
+    return result;
   }
 
   static isvalid(csvString, options = DEFAULT_OPTION) {
     //para ver o conteudo do arquivo
     // fs.readFileSync('./mocks/threeItems-valid.csv', 'utf8')
-    const [headers, ...fileWithoutHeader] = csvString.split(/\r?\n/)
-    if(!fileWithoutHeader.lenght){
+    const [header, ...fileWithoutHeader] = csvString.split(/\r?\n/);
+    const isHeaderValid = header === options.fields.join(",");
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+    if (
+      !fileWithoutHeader.length ||
+      fileWithoutHeader.length > options.maxLines
+    ) {
       return {
         error: error.FILE_LENGTH_ERROR_MESSAGE,
-        valid: false
-      }
+        valid: false,
+      };
     }
-    console.log(headers,fileWithoutHeader);
+    return { valid: true };
+  }
+
+  static parseCsvToJson(csvString) {
+    const lines = csvString.split(/\r?\n/);
+    //remover a primeira linha = header
+    const firstLine = lines.shift();
+    const header = firstLine.split(",");
+    const users = lines.map((line) => {
+      const columns = line.split(",");
+      const user = {};
+      for (const index in columns) {
+        user[header[index]] = columns[index].trim();
+      }
+      return user;
+    });
+
+    return users;
   }
 }
 
-module.exports = File
+module.exports = File;
