c298e500fa05ff968ecb99d2a5332f7403dcfcc9
Guilherme Alacoc <guilhermealacoc@gmail.com>
2024-08-08 19:22:45 -0300

    feat: adicionanndo testes finais em API

--- modulo01/aula04-e2e-coverage/src/api.js
+++ modulo01/aula04-e2e-coverage/src/api.js
@@ -1,46 +1,45 @@
 const http = require('http')
-
 const DEFAULT_USER = {
-    username: 'usuario',
-    password: '123'
+  username: 'ErickWendel',
+  password: '123'
 }
-
-const {once} = require('events')
-
+const { once } = require('events')
 const routes = {
-    '/contact:get': (request, response) => {
-        response.write('contact us page')
-        return response.end()
-    },
-    //curl -X POST --data '{"username":"user","password":"123"}' localhost:3000/login
-    //curl -X POST --data '{"username":"usuario","password":"123"}' localhost:3000/login
-    '/login:post': async (request, response) => {
-    const user = JSON.parse(await once (request, "data"))
+  '/contact:get': (request, response) => {
+    response.write('contact us page')
+    return response.end()
+  },
+  // curl -i -X POST --data '{"username": "erickwendel","password": "23"}' localhost:3000/login
+  // curl -i -X POST --data '{"username": "Erickwendel","password": "123"}' localhost:3000/login
+  '/login:post': async (request, response) => {
+    const user = JSON.parse(await once(request, "data"))
     const toLower = (text) => text.toLowerCase()
-    if(
-        toLower(user.username) !== toLower( DEFAULT_USER.username )||
-        user.password !== DEFAULT_USER.password
-    ){
-        response.writeHead(401)
-        response.end('Login Falhou')
-        return
+    if (
+      toLower(user.username) !== toLower(DEFAULT_USER.username) ||
+      user.password !== DEFAULT_USER.password
+    ) {
+      response.writeHead(401)
+      response.end("Log in failed!")
+      return
     }
-    return response.end('Sucesso!!!!')
-    },
-    default(request, response) {
-        response.writeHead(404)
-        return response.end('not found!')
-    }
+
+    return response.end("Log in succeeded!")
+  },
+  default(request, response) {
+    response.writeHead(404)
+    return response.end('not found!')
+  }
 }
 
-function handler(request, response){
-    const {url, method} = request
-    const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`
-    const chosen = routes[routeKey] || routes.default
-    return chosen(request, response)
+function handler(request, response) {
+  const { url, method } = request
+  const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`
+  const chosen = routes[routeKey] || routes.default
+  return chosen(request, response)
 }
 
-const app = http.createServer(handler).listen(3000, ()=> console.log('running at 3000'))
+const app = http.createServer(handler)
+  .listen(3000, () => console.log('running at 3000'))
 
 
 module.exports = app
